937f59564194cc1d1afba222b0f7a65c61a61d84
gcamposl <gcamposlopes@gmail.com>
2023-07-26 09:28:13 -0300

    packages

---
+++ 1 - packages/auxiliar/auxiliar.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// toda func exportada deve ter comentarios, w - private / W - public
+func Write() {
+	fmt.Println("writing from file auxiliar")
+	write2()
+}
---
+++ 1 - packages/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main()	{
+	fmt.Println("writing from file main")
+	auxiliar.Write()
+	//auxiliar.write2()
+}
