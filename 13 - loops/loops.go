840f6d26c9f18569cb461d0ad188d33cb173011d
gcamposl <gcamposlopes@gmail.com>
2023-08-08 23:01:23 -0300

    loops

---
+++ 13 - loops/loops.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Loops")
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("Index:", i)
+		//time.Sleep(time.Second)
+	}
+
+	j := 5
+	for j < 11 { // like while
+		fmt.Println("Index:", j)
+		j++
+		//time.Sleep(time.Second)
+	}
+
+	names := [3]string{"abc", "cba", "bca"}
+	for index, name := range names {
+		fmt.Println(index, ":", name)
+	}
+
+	for _, name := range names { // without index
+		fmt.Println(name)
+	}
+
+	for _, letter := range "letters" { // without index
+		fmt.Println(string(letter))
+	}
+
+	user := map[string]string{
+		"name":     "test",
+		"lastName": "escescescesc",
+	}
+
+	for key, value := range user {
+		fmt.Println(key, value)
+	}
+}
