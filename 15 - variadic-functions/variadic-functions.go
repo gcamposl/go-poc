cfaf77a6e76c94efca592bfe4bf481f50e46e332
gcamposl <gcamposlopes@gmail.com>
2023-08-09 23:18:56 -0300

    variadic funtions pt2

--- 15 - variadic-functions/variadic-functions.go
+++ 15 - variadic-functions/variadic-functions.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// numbers ...int == slice
 func calculate(numbers ...int) {
 	total := 0
 	for _, number := range numbers {
@@ -10,7 +11,16 @@ func calculate(numbers ...int) {
 	fmt.Println(total)
 }
 
+func write(text string, numbers ...int) {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	fmt.Println(text, total)
+}
+
 func main() {
 	calculate(1, 2, 3)
 	calculate(1, 2, 3, 4)
+	write("test", 1, 2, 3)
 }
