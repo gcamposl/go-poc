f32bc1e01bf644d33c697fb53b7b60223ae5c50f
gcamposl <gcamposlopes@gmail.com>
2023-08-12 17:37:04 -0300

    defer

---
+++ 18 - defer/defer.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func function1() {
+	fmt.Println("execution 1")
+}
+
+func function2() {
+	fmt.Println("execution 2")
+}
+
+func isAproved(n1, n2 float32) bool {
+	defer fmt.Println("calcuted average. wait for the result")
+	if media := (n1 + n2) / 2; media >= 6 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println("defer")
+	defer function1()
+	function2()
+
+	result := isAproved(5, 7)
+	if result {
+		fmt.Println("aproved!")
+	} else {
+		fmt.Println("reproved!")
+	}
+}
