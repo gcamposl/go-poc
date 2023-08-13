bdf1bd4fef572d4b517ffa7d25df8119948054d1
gcamposl <gcamposlopes@gmail.com>
2023-08-13 10:39:46 -0300

    panic and recover pt2

--- panic-recover/panic-recover.go
+++ panic-recover/panic-recover.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+func tryRecoverExecution() {
+	if r := recover(); r != nil {
+		fmt.Println("attempt to recover execution")
+	}
+}
+
 func isAproved(n1, n2 float32) bool {
+	defer tryRecoverExecution()
 	average := (n1 + n2) / 2
 	if average > 6 {
 		return true
@@ -15,6 +22,6 @@ func isAproved(n1, n2 float32) bool {
 
 func main() {
 	fmt.Println("panic and recover")
-	fmt.Println(isAproved(6, 5))
+	fmt.Println(isAproved(6, 6))
 	fmt.Println("pos execution")
 }
