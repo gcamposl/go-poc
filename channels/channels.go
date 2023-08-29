30a9cb3413d1f31ab138859a8974785eb4a2e149
gcamposl <gcamposlopes@gmail.com>
2023-08-29 20:47:15 -0300

    channels pt1

--- channels/channels.go
+++ channels/channels.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func write(text string, channel chan string) {
+	time.Sleep(time.Second * 5)
+	for i := 0; i < 5; i++ {
+		channel <- text
+		time.Sleep(time.Second)
+	}
+}
 
 func main() {
 	fmt.Println("channels")
+	channel := make(chan string)
+
+	go write("hello", channel)
+
+	fmt.Println("after")
+	message := <-channel // receive channel
+	fmt.Println(message)
 }
