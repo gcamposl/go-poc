811f55b679bd794d6bf700b4d22d27503a97cfc9
gcamposl <gcamposlopes@gmail.com>
2023-09-19 23:31:17 -0300

    pattern generator

--- competitions/competition-standards/pattern-generator/pattern-generator.go
+++ competitions/competition-standards/pattern-generator/pattern-generator.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	channel := write("helloooooooooooo")
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-channel)
+	}
+}
+
+func write(text string) <-chan string {
+	channel := make(chan string)
+	go func() {
+		for {
+			channel <- fmt.Sprintf("received value: %s", text)
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+	return channel
 }
