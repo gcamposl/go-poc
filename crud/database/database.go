8ecfa19f80c7f6346818480c31e0d4807d26e0ab
gcamposl <gcamposlopes@gmail.com>
2024-07-30 22:06:50 -0300

    crud

---
+++ crud/database/database.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" // connection driver with mysql
+)
+
+// opens connection to the database
+func Connect() (*sql.DB, error) {
+	connectionString := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
