6527f0b56422df7fe9859d067c46bd322e2fcafe
gcamposl <gcamposlopes@gmail.com>
2024-07-31 22:05:13 -0300

    create server

--- crud/main.go
+++ crud/main.go
@@ -11,6 +11,10 @@ import (
 func main() {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+
+	}).Methods(http.MethodPost)
+
 	fmt.Println("Listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
