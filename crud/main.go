a68f90f0ef16a646837fa13083a4b838bf6c9529
gcamposl <gcamposlopes@gmail.com>
2024-08-02 19:47:59 -0300

    crud 2

--- crud/main.go
+++ crud/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crud/server"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,9 +12,7 @@ import (
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-
-	}).Methods(http.MethodPost)
+	router.HandleFunc("/users", server.CreateUser).Methods(http.MethodPost)
 
 	fmt.Println("Listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- crud/server/server.go
+++ crud/server/server.go
@@ -1,7 +1,14 @@
 package server
 
-import "net/http"
+import (
+	"io/ioutil"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-
+	request, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.Write([]byte("failed to read body request!"))
+		return
+	}
 }
