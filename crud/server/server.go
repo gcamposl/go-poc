1e33c5e554cdb75695578e82fdef714b2f74c799
gcamposl <gcamposlopes@gmail.com>
2024-08-12 22:07:23 -0300

    create user

--- crud/server/server.go
+++ crud/server/server.go
@@ -1,14 +1,31 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type user struct {
+	ID    uint32 `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// insert user into database
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	request, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.Write([]byte("failed to read body request!"))
 		return
 	}
+
+	var user user
+	if err = json.Unmarshal(request, &user); err != nil {
+		w.Write([]byte("failed to converter user in struct"))
+		return
+	}
+
+	fmt.Println(user)
 }
